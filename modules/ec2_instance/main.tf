f2e3c01a8509ef3ef0b2d72c661b22f9a0bf5718
DevaKune <53827543+deva9017@users.noreply.github.com>
2024-12-30 14:04:22 +0530

    Create main.tf

---
+++ modules/ec2_instance/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "this" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+}
+
+output "instance_id" {
+  value = aws_instance.this.id
+}
+
+output "public_ip" {
+  value = aws_instance.this.public_ip
+}
