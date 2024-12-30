959ec43c860bf6f742a7bf55745629cf3c698556
DevaKune <53827543+deva9017@users.noreply.github.com>
2024-12-30 13:32:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.0.0"
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "my-terraform-states" # Single S3 bucket for all workspaces
+    key            = "terraform/${terraform.workspace}/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 }
 
 module "s3_bucket" {
