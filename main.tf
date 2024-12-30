eb2fe34b87e5bc74d5bfcc8a7d3f71fd01c14dbf
DevaKune <53827543+deva9017@users.noreply.github.com>
2024-12-30 14:09:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket         = "my-terraform-states-158"
-    key            = "terraform/${terraform.workspace}/terraform.tfstate"
+    key            = "terraform/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
