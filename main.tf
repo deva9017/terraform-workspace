6fb49607862bd4da34e91fbdbb968427f294b743
DevaKune <53827543+deva9017@users.noreply.github.com>
2024-12-30 14:03:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,30 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Configure the backend to use S3 for state storage
 terraform {
-  required_version = ">= 1.0.0"
   backend "s3" {
-    bucket         = "my-terraform-states-158" # Single S3 bucket for all workspaces
-    key            = "terraform/terraform.tfstate"
+    bucket         = "my-terraform-states-158"
+    key            = "terraform/${terraform.workspace}/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
 
-module "s3_bucket" {
-  source      = "./modules/s3_bucket"
-  bucket_name = var.bucket_name
-  tags        = var.tags
+# Call EC2 Instance module
+module "ec2_instance" {
+  source = "./modules/ec2_instance"
+  instance_type = var.instance_type
+  ami            = var.ami_id
+  key_name       = var.key_name
+}
+
+output "ec2_instance_id" {
+  value = module.ec2_instance.instance_id
+}
+
+output "ec2_public_ip" {
+  value = module.ec2_instance.public_ip
 }
